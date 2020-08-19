c8a1ada12865013711fe5a995615a05020517a70
Thanh Tung Nguyen <65433836+tungstudies@users.noreply.github.com>
2020-08-19 17:20:02 -0400

    Update docker-install.sh
    add a command to create docker group

--- docker-install.sh
+++ docker-install.sh
@@ -15,5 +15,8 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get -y install docker-engine docker-compose
 
+# create docker group
+sudo groupadd docker
+
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
